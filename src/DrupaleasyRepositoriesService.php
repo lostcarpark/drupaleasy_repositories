535b70e774bed69c7b38dcda926cf5ea995f5b56
James Shields <james@lostcarpark.com>
2023-03-29 12:30:50 +0100

    Initial commit.

---
+++ src/DrupaleasyRepositoriesService.php
@@ -0,0 +1,365 @@
+<?php
+
+namespace Drupal\drupaleasy_repositories;
+
+use Drupal\Component\Plugin\PluginManagerInterface;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+
+/**
+ * Service description.
+ */
+class DrupaleasyRepositoriesService {
+  use StringTranslationTrait;
+
+  /**
+   * The plugin manager interface.
+   *
+   * @var \Drupal\Component\Plugin\PluginManagerInterface
+   */
+  protected PluginManagerInterface $pluginManagerDrupaleasyRepositories;
+
+  /**
+   * The config factory interface.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected ConfigFactoryInterface $configFactory;
+
+  /**
+   * The entity type manager.
+   *
+   * @var EntityTypeManagerInterface
+   */
+  protected EntityTypeManagerInterface $entityTypeManager;
+
+  /**
+   * The dry-run parameter.
+   *
+   * When set to "true", no nodes are created, updated, or deleted.
+   *
+   * @var bool
+   */
+  protected bool $dryRun = FALSE;
+
+  /**
+   * Constructs DrupaleasyRepositories object.
+   *
+   * @param \Drupal\Component\Plugin\PluginManagerInterface $plugin_manager_drupaleasy_repositories
+   *   The plugin manager interface.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The configuration factory interface.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   * @param bool $dry_run
+   *   The dry run parameter specifies whether changes should be changed.
+   */
+  public function __construct(
+    PluginManagerInterface $plugin_manager_drupaleasy_repositories,
+    ConfigFactoryInterface $config_factory,
+    EntityTypeManagerInterface $entity_type_manager,
+    bool $dry_run
+  ) {
+    $this->pluginManagerDrupaleasyRepositories = $plugin_manager_drupaleasy_repositories;
+    $this->configFactory = $config_factory;
+    $this->entityTypeManager = $entity_type_manager;
+    $this->dryRun = $dry_run;
+  }
+
+  /**
+   * Get the list of Repository plugins and filter out any that aren't enabled.
+   *
+   * @return array
+   */
+  public function getEnabledRepositoryPlugins(): array {
+    $repository_plugin_ids = $this->configFactory->get('drupaleasy_repositories.settings')->get('repositories') ?? [];
+    return array_filter($repository_plugin_ids, fn ($item) => !empty($item));
+  }
+
+  /**
+   * Get the validator help text for all plugins.
+   *
+   * @return string
+   *   The help text.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\PluginException;
+   */
+  public function getValidatorHelpText(): string {
+    $repository_plugins = [];
+    // Determine which plugins are enabled.
+    $enabled_repository_plugin_ids = $this->getEnabledRepositoryPlugins();
+    foreach ($enabled_repository_plugin_ids as $enabled_repository_plugin_id) {
+      if (!empty($enabled_repository_plugin_id)) {
+        $repository_plugins[] = $this->pluginManagerDrupaleasyRepositories->createInstance($enabled_repository_plugin_id);
+      }
+    }
+
+    $help = [];
+    foreach ($repository_plugins as $repository_plugin) {
+      $help[] = $repository_plugin->validateHelpText();
+    }
+
+    if (count($help)) {
+      return implode(' ', $help);
+    }
+    return '';
+  }
+
+  /**
+   * Validate repository URLs.
+   *
+   * Validate the URLs are valid based on the enabled plugins and ensure they
+   * haven't been added by another user.
+   *
+   * @param array $urls
+   *   The urls to be validated.
+   * @param int $uid
+   *   The user id of the user submitting the URLs.
+   *
+   * @return string
+   *   Errors reported by plugins.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\PluginException;
+   */
+  public function validateRepositoryUrls(array $urls, int $uid): string {
+    $errors = [];
+    $repository_plugins = [];
+
+    // Get IDs of enabled DrupaleasyRepository plugins.
+    $enabled_repository_plugin_ids = $this->getEnabledRepositoryPlugins();
+    if (empty($enabled_repository_plugin_ids)) {
+      return 'There are no enabled repository plugins';
+    }
+
+    // Instantiate each enabled DrupaleasyRepository plugin.
+    foreach ($enabled_repository_plugin_ids as $enabled_repository_plugin_id) {
+      if (!empty($enabled_repository_plugin_id)) {
+        $repository_plugins[] = $this->pluginManagerDrupaleasyRepositories->createInstance($enabled_repository_plugin_id);
+      }
+    }
+
+    // Loop around each Repository URL and attempt to validate.
+    foreach ($urls as $url) {
+      if (is_array($url)) {
+        if ($uri = trim($url['uri'])) {
+          $validated = FALSE;
+          // Check to see if the URI is valid for any enabled plugins.
+          /** @var \Drupal\drupaleasy_repositories\DrupaleasyRepositories\DrupaleasyRepositoriesInterface $repository_plugin */
+          foreach ($repository_plugins as $repository_plugin) {
+            if ($repository_plugin->validate($uri)) {
+              $validated = TRUE;
+              $repo_metadata = $repository_plugin->getRepo($uri);
+              if ($repo_metadata) {
+                if (!$this->isUnique($repo_metadata, $uid)) {
+                  $errors[] = $this->t('The repository at %uri has been added by another user.', ['%uri' => $uri]);
+                }
+              }
+              else {
+                $errors[] = $this->t('The repository at the url %uri was not found.', ['%uri' => $uri]);
+              }
+              break;
+            }
+          }
+          if (!$validated) {
+            $errors[] = $this->t('The repository url @uri is not valid.', ['@uri' => $uri]);
+          }
+        }
+      }
+    }
+
+    if ($errors) {
+      return implode(' ', $errors);
+    }
+    // No errors found.
+    return '';
+  }
+
+  /**
+   * Update the repository nodes for a given account.
+   *
+   * @param \Drupal\Core\Entity\EntityInterface $account
+   *   The user account whose repositories to update.
+   *
+   * @return bool
+   *   TRUE if successful.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\PluginException;
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   */
+  public function updateRepositories(EntityInterface $account): bool {
+    $repos_metadata = [];
+    $enabled_repository_plugin_ids = $this->getEnabledRepositoryPlugins();
+
+    foreach ($enabled_repository_plugin_ids as $enabled_repository_plugin_id) {
+      if (!empty($enabled_repository_plugin_id)) {
+        /** @var \Drupal\drupaleasy_repositories\DrupaleasyRepositories\DrupaleasyRepositoriesInterface $repository_location */
+        $repository_location = $this->pluginManagerDrupaleasyRepositories->createInstance($enabled_repository_plugin_id);
+        // Loop through repository URLs.
+        foreach ($account->field_repository_url ?? [] as $url) {
+          // Check if the URL validates for this repository.
+          if ($repository_location->validate($url->uri)) {
+            // Confirm the repository exists and get metadata.
+            if ($repo_metadata = $repository_location->getRepo($url->uri)) {
+              $repos_metadata += $repo_metadata;
+            }
+          }
+        }
+      }
+    }
+    return $this->updateRepositoryNodes($repos_metadata, $account) && $this->deleteRepositoryNodes($repos_metadata, $account);
+  }
+
+  /**
+   * Update repository nodes for a given user.
+   *
+   * @param array $repos_info
+   *   Repository info from API call.
+   * @param \Drupal\Core\Entity\EntityInterface $account
+   *   The user account whose repositories to update.
+   *
+   * @return bool
+   *   TRUE if successful.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   */
+  protected function updateRepositoryNodes(array $repos_info, EntityInterface $account): bool {
+    if (!$repos_info) {
+      return TRUE;
+    }
+    // Prepare the storage and query stuff.
+    $node_storage = $this->entityTypeManager->getStorage('node');
+    foreach ($repos_info as $key => $info) {
+      // Calculate the hash value.
+      $hash = md5(serialize($info));
+
+      // Look for repository nodes from this user with matching
+      // machine_name.
+      /** @var \Drupal\Core\Entity\Query\QueryInterface $query */
+      $query = $node_storage->getQuery();
+      $query->condition('type', 'repository')
+        ->condition('uid', $account->id())
+        ->condition('field_machine_name', $key)
+        ->condition('field_source', $info['source'])
+        ->accessCheck(FALSE);
+      $results = $query->execute();
+
+      if ($results) {
+        /** @var \Drupal\node\Entity\Node $node */
+        $node = $node_storage->load(reset($results));
+
+        if ($hash != $node->get('field_hash')->value) {
+          // Something changed, update node.
+          $node->setTitle($info['label']);
+          $node->set('field_description', $info['description']);
+          $node->set('field_machine_name', $key);
+          $node->set('field_number_of_issues', $info['num_open_issues']);
+          $node->set('field_source', $info['source']);
+          $node->set('field_url', $info['url']);
+          $node->set('field_hash', $hash);
+          if (!$this->dryRun) {
+            $node->save();
+            //$this->repoUpdated($node, 'updated');
+          }
+        }
+      }
+      else {
+        // Repository node doesn't exist - create a new one.
+        /** @var \Drupal\node\Entity\Node $node */
+        $node = $node_storage->create([
+          'uid' => $account->id(),
+          'type' => 'repository',
+          'title' => $info['label'],
+          'field_description' => $info['description'],
+          'field_machine_name' => $key,
+          'field_number_of_issues' => $info['num_open_issues'],
+          'field_source' => $info['source'],
+          'field_url' => $info['url'],
+          'field_hash' => $hash,
+        ]);
+        if (!$this->dryRun) {
+          $node->save();
+          //$this->repoUpdated($node, 'created');
+        }
+      }
+    }
+    return TRUE;
+  }
+
+  /**
+   * Delete repository nodes deleted from the source for a given user.
+   *
+   * @param array<string, array<string, string>> $repos_info
+   *   Repository info from API call.
+   * @param \Drupal\Core\Entity\EntityInterface $account
+   *   The user account whose repositories to update.
+   *
+   * @return bool
+   *   TRUE if successful.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   */
+  protected function deleteRepositoryNodes(array $repos_info, EntityInterface $account): bool {
+    // Prepare the storage and query stuff.
+    /** @var \Drupal\Core\Entity\EntityStorageInterface $node_storage */
+    $node_storage = $this->entityTypeManager->getStorage('node');
+
+    /** @var \Drupal\Core\Entity\Query\QueryInterface $query */
+    $query = $node_storage->getQuery();
+    $query->condition('type', 'repository')
+      ->condition('uid', $account->id())
+      ->accessCheck(FALSE);
+    // We can't chain this above because $repos_info might be empty.
+    if ($repos_info) {
+      $query->condition('field_machine_name', array_keys($repos_info), 'NOT IN');
+    }
+    $results = $query->execute();
+    if ($results) {
+      $nodes = $node_storage->loadMultiple($results);
+      /** @var \Drupal\node\Entity\Node $node */
+      foreach ($nodes as $node) {
+        if (!$this->dryRun) {
+          $node->delete();
+          //$this->repoUpdated($node, 'deleted');
+        }
+      }
+    }
+    return TRUE;
+  }
+
+  /**
+   * Check to see if a given repository is unique.
+   *
+   * @param array $repo_info
+   *   Repository to check.
+   * @param int $uid
+   *   UID of user submitting.
+   *
+   * @return bool
+   *   Return true if repository is unique to user.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   */
+  protected function isUnique(array $repo_info, int $uid): bool {
+    /** @var \Drupal\node\NodeStorageInterface $node_storage */
+    $node_storage = $this->entityTypeManager->getStorage('node');
+    $repo_metadata = array_pop($repo_info);
+
+    $query = $node_storage->getQuery();
+    $results = $query->condition('type', 'repository')
+      ->condition('field_url', $repo_metadata['url'])
+      ->condition('uid', $uid, '<>')
+      ->accessCheck(FALSE)
+      ->execute();
+
+    // Return true if no result found.
+    return !count($results);
+  }
+
+}
---
+++ tests/src/Functional/AddYmlRepoTest.php
@@ -0,0 +1,232 @@
+<?php
+
+namespace Drupal\Tests\drupaleasy_repositories\Functional;
+
+use Behat\Mink\Exception\ExpectationException;
+use Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException;
+use Drupal\Component\Plugin\Exception\PluginNotFoundException;
+use Drupal\Core\Entity\EntityDisplayRepository;
+use Drupal\Core\Entity\EntityStorageException;
+use Drupal\Core\Extension\Extension;
+use Drupal\Core\Extension\ModuleHandler;
+use Drupal\node\NodeInterface;
+use Drupal\Tests\BrowserTestBase;
+use Drupal\Tests\drupaleasy_repositories\Traits\RepositoryContentTypeTrait;
+use Drupal\Tests\WebAssert;
+
+/**
+ * Test description.
+ *
+ * @group drupaleasy_repositories
+ */
+class AddYmlRepoTest extends BrowserTestBase {
+
+  use RepositoryContentTypeTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'stark';
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'drupaleasy_repositories',
+  ];
+
+  /**
+   * {@inheritdoc}
+   * @throws EntityStorageException
+   */
+  protected function setUp(): void {
+    parent::setUp();
+    $config = $this->config(name: 'drupaleasy_repositories.settings');
+    $config->set('repositories', ['yml_remote' => 'yml_remote']);
+    $config->save();
+
+    // Create and login as a Drupal admin user with permission to access
+    // the DrupalEasy Repositories Settings page. This is UID=2 because UID=1
+    // is created by FunctionalTestSetupTrait. The root user can be accessed
+    // with $this->rootUser.
+    $admin_user = $this->drupalCreateUser(['configure drupaleasy repositories']);
+    $this->drupalLogin($admin_user);
+
+    // $this->createRepositoryContentType();
+    // // Create User entity Repository URL field.
+    // FieldStorageConfig::create([
+    //   'field_name' => 'field_repository_url',
+    //   'type' => 'link',
+    //   'entity_type' => 'user',
+    //   'cardinality' => -1,
+    // ])->save();
+    // FieldConfig::create([
+    //   'field_name' => 'field_repository_url',
+    //   'entity_type' => 'user',
+    //   'bundle' => 'user',
+    //   'label' => 'Repository URL',
+    // ])->save();
+
+    /** @var EntityDisplayRepository $entity_display_repository  */
+    $entity_display_repository = \Drupal::service('entity_display.repository');
+    $entity_display_repository->getFormDisplay('user', 'user', 'default')
+      ->setComponent('field_repository_url', ['type' => 'link_default'])
+      ->save();
+  }
+
+  /**
+   * Test that the settings page can be reached and works as expected.
+   *
+   * This tests that an admin user can access the settings page, select a
+   * plugin to enable, and submit the page successfully.
+   *
+   * @test
+   * @throws ExpectationException
+   */
+  public function testSettingsPage(): void {
+    // Start the browsing session.
+    $session = $this->assertSession();
+
+    // Navigate to the DrupalEasy Repositories Settings page and confirm we
+    // can reach it.
+    $this->drupalGet('/admin/config/services/repositories');
+    // Try this with a 500 status code to see it fail.
+    $session->statusCodeEquals(200);
+    $session->titleEquals('DrupalEasy repositories settings | Drupal');
+    $session->pageTextContains('DrupalEasy repositories settings');
+
+    // Select the "Remote .yml file" checkbox and submit the form.
+    $edit = [
+      'edit-repositories-yml-remote' => 'yml_remote',
+    ];
+    $this->submitForm($edit, 'Save configuration');
+    $session->statusCodeEquals(200);
+    $session->responseContains('The configuration options have been saved.');
+    $session->checkboxChecked('edit-repositories-yml-remote');
+    // $session->checkboxNotChecked('edit-repositories-github');
+  }
+
+  /**
+   * Create user and log in.
+   *
+   * @return WebAssert
+   *   The current browser session.
+   *
+   * @throws EntityStorageException
+   * @throws ExpectationException
+   */
+  public function createAndLoginUser(): WebAssert {
+    // Create and login as a Drupal user with permission to access
+    // content.
+    $user = $this->drupalCreateUser(['access content']);
+    $this->drupalLogin($user);
+
+    $session = $this->assertSession();
+
+    // Navigate to their edit profile page and confirm we can reach it.
+    $this->drupalGet('/user/' . $user->id() . '/edit');
+    // Try this with a 500 status code to see it fail.
+    $session->statusCodeEquals(200);
+
+    return $session;
+  }
+
+  /**
+   * Get the full path to the module directory.
+   *
+   * @return string
+   */
+  public function getModuleFullPath(): string {
+    // Get the full path to the test .yml file.
+    /** @var ModuleHandler $module_handler */
+    $module_handler = \Drupal::service('module_handler');
+    /** @var Extension $module */
+    $module = $module_handler->getModule('drupaleasy_repositories');
+    return \Drupal::request()->getUri() . $module->getPath();
+  }
+
+  /**
+   * Submit user profile form to save repository.
+   *
+   * @param WebAssert $session
+   *   The current user session.
+   * @param string $url
+   *   The URL to save.
+   *
+   * @throws ExpectationException
+   */
+  public function saveRepository(WebAssert $session, string $url): void {
+    $edit = [
+      'field_repository_url[0][uri]' => $url,
+    ];
+    $this->submitForm($edit, 'Save');
+    $session->statusCodeEquals(200);
+    $session->responseContains('The changes have been saved.');
+  }
+
+  /**
+   * Test that a yml repo can be added to profile by a user.
+   *
+   * This tests that a yml-based repo can be added to a user's profile and
+   * that a repository node is successfully created upon saving the profile.
+   *
+   * @test
+   * @throws EntityStorageException
+   * @throws ExpectationException
+   * @throws PluginNotFoundException
+   * @throws InvalidPluginDefinitionException
+   */
+  public function testAddYmlRepo(): void {
+    $session = $this->createAndLoginUser();
+
+    // Add the test .yml file path and submit the form.
+    $this->saveRepository($session, $this->getModuleFullPath() . '/tests/assets/batman-repo.yml');
+
+    // We can't check for the following message unless we also have the future
+    // drupaleasy_notify module enabled.
+    // $session->responseContains('The repo named <em class="placeholder">The Batman repository</em> has been created');.
+    // Find the new repository node.
+    /** @var \Drupal\Core\Entity\Query\QueryInterface $query */
+    $query = \Drupal::entityQuery('node');
+    $query->condition('type', 'repository')->accessCheck(FALSE);
+    $results = $query->execute();
+    $session->assert(count($results) === 1, 'Either 0 or more than 1 repository nodes were found.');
+
+    $entity_type_manager = \Drupal::entityTypeManager();
+    $node_storage = $entity_type_manager->getStorage('node');
+    /** @var NodeInterface $node */
+    $node = $node_storage->load(reset($results));
+
+    // Check values.
+    $session->assert($node->field_machine_name->value == 'batman-repo', 'Machine name does not match.');
+    $session->assert($node->field_source->value == 'yml_remote', 'Source does not match.');
+    $session->assert($node->title->value == 'The Batman repository', 'Label does not match.');
+    $session->assert($node->field_description->value == 'This is where Batman keeps all his crime-fighting code.', 'Description does not match.');
+    $session->assert($node->field_number_of_issues->value == '6', 'Number of issues does not match.');
+  }
+
+  /**
+   * Test that a yml repo can be removed from profile by a user.
+   *
+   * @return void
+   * @throws ExpectationException
+   * @throws EntityStorageException
+   */
+  function testRemoveYmlRepo(): void {
+    $session = $this->createAndLoginUser();
+
+    // Add the test .yml file path and submit the form.
+    $this->saveRepository($session, $this->getModuleFullPath(). '/tests/assets/batman-repo.yml');
+
+    // Add the test .yml file path and submit the form.
+    $this->saveRepository($session, '');
+
+    /** @var \Drupal\Core\Entity\Query\QueryInterface $query */
+    $query = \Drupal::entityQuery('node');
+    $query->condition('type', 'repository')->accessCheck(FALSE);
+    $results = $query->execute();
+    // Verify that no results returned.
+    $session->assert(count($results) === 0, 'The repository was not deleted.');
+  }
+
+}
